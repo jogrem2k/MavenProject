eeeaa6698f672e497c932d38a21b7a403d4e37e6
David <David@DESKTOP-DTHRTNU>
2021-03-05 21:08:24 -0500

    -Update Suite1.java

--- Suite1.java
+++ Suite1.java
@@ -18,6 +18,7 @@ public class Suite1 {
 		 driver.manage().window().maximize();
 		 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		 driver.get("http://magento.com");
+		 
 		 driver.findElement(By.xpath("//*[@id=\"block-header\"]/ul/li[9]/a/span[1]/div")).click();
 		 driver.findElement(By.id("register")).click();
 		 driver.findElement(By.id("firstname")).sendKeys("Joseph");
